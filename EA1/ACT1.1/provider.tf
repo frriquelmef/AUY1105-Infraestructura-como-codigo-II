bc0b0e29bb6b2a480caabaccf992e37dd740e7f8
PAAPCA02TR0512 <Duoc@PAAPCA02TR0512.duoc.acad>
2026-03-11 20:01:14 -0300

    Pequeño cambio sugerido

--- EA1/ACT1.1/provider.tf
+++ EA1/ACT1.1/provider.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
